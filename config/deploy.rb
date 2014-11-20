6e10f8dee7802494897ff10b4e23b4eb804bde6b
abouvier <abouvier@currentmarketing.com>
2014-11-20 13:48:14 -0500

    Updating .env

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :deploy_to, '/home/websites/cap'
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml shared/.env}
+set :linked_files, %w{config/database.yml .env}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
