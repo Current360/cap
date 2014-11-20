94b385781a3ecb1d717543f7fbe702e558710c78
abouvier <abouvier@currentmarketing.com>
2014-11-20 10:26:08 -0500

    capistrano setup testing is complete

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, 'git@github.com:Current360/cap.git'
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
 # Default deploy_to directory is /var/www/my_app
-# set :deploy_to, '/var/www/my_app'
+set :deploy_to, '/home/websites/cap'
 
 # Default value for :scm is :git
 # set :scm, :git
